e49ca32e06ec4b5f6de34dbe7890dc16fc852a47
erlel <erlel@Ermir-PC.fritz.box>
2022-12-08 14:07:14 +0100

    Nächster Screen

--- Woofers/src/frontend/mitarbieterlisteTermin/Mitarbeiterliste.java
+++ Woofers/src/frontend/mitarbieterlisteTermin/Mitarbeiterliste.java
@@ -1,8 +1,6 @@
 package frontend.mitarbieterlisteTermin;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -10,65 +8,52 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
 
-import frontend.terminal.Hauptfenster;
+import view.View;
 
-public class Mitarbeiterliste {
+public class Mitarbeiterliste implements View {
 
-	protected Shell shlMehrereGleichnamigeKunden;
+	private Shell shell;
+	private Button btnNewButton;
+	private Label lblWelchenGenauMeinen;
+	private List list;
+	private Label lblKundenid;
+	private Label lblX;
 
-	/**
-	 * Launch the application..
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			Mitarbeiterliste window = new Mitarbeiterliste();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public Mitarbeiterliste(Shell shell) {
+		this.shell = shell;
+		assignElements();
 	}
 
-	/**
-	 * Open the window.
-	 */
 	public void open() {
 		Display display = Display.getDefault();
-		createContents();
-		shlMehrereGleichnamigeKunden.open();
-		shlMehrereGleichnamigeKunden.layout();
-		while (!shlMehrereGleichnamigeKunden.isDisposed()) {
+		init();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 	}
 
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		shlMehrereGleichnamigeKunden = new Shell();
-		shlMehrereGleichnamigeKunden.setTouchEnabled(true);
-		shlMehrereGleichnamigeKunden.setBackground(SWTResourceManager.getColor(64, 0, 128));
-		shlMehrereGleichnamigeKunden.setSize(497, 462);
-		shlMehrereGleichnamigeKunden.setText("Mehrere gleichnamige Kunden");
+	@Override
+	public void init() {
+		initShell();
+		initLabel();
+		initButtons();
+		initList();
 
-		Button btnNewButton = new Button(shlMehrereGleichnamigeKunden, SWT.NONE);
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseDown(MouseEvent e) {
-				Hauptfenster.main(null);
-				shlMehrereGleichnamigeKunden.dispose();
+	}
 
-			}
-		});
-		btnNewButton.setFont(SWTResourceManager.getFont("Arial", 9, SWT.NORMAL));
-		btnNewButton.setText("Zur\u00FCck zum Hauptemen\u00FC");
-		btnNewButton.setBounds(111, 348, 271, 25);
+	private void initShell() {
+		shell.setTouchEnabled(true);
+		shell.setBackground(SWTResourceManager.getColor(64, 0, 128));
+		shell.setSize(497, 462);
+		shell.setText("Mehrere gleichnamige Kunden");
+	}
 
-		Label lblWelchenGenauMeinen = new Label(shlMehrereGleichnamigeKunden, SWT.NONE);
+	private void initLabel() {
+		// I. Label: "Terminliste"
 		lblWelchenGenauMeinen.setForeground(SWTResourceManager.getColor(255, 128, 0));
 		lblWelchenGenauMeinen.setBackground(SWTResourceManager.getColor(64, 0, 128));
 		lblWelchenGenauMeinen.setText("Terminliste");
@@ -76,20 +61,7 @@ public class Mitarbeiterliste {
 		lblWelchenGenauMeinen.setAlignment(SWT.CENTER);
 		lblWelchenGenauMeinen.setBounds(100, 119, 272, 38);
 
-		List list = new List(shlMehrereGleichnamigeKunden, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		list.setTouchEnabled(true);
-		list.setBounds(65, 179, 359, 136);
-		list.setItems(new String[] { "1. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Bello",
-				"2. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Bello",
-				"3. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Werner",
-				"4. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Bello",
-				"5. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Bello",
-				"6. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Wiener",
-				"7. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Wiener",
-				"8. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Wiener",
-				"9. Termin am DD.MM.YYYY um HH:MM Uhr bis HH:MM Uhr | Hund: Wiener" });
-
-		Label lblKundenid = new Label(shlMehrereGleichnamigeKunden, SWT.NONE);
+		// II. Label: "Mitarbeiter"
 		lblKundenid.setText("Mitarbeiter");
 		lblKundenid.setForeground(SWTResourceManager.getColor(255, 128, 0));
 		lblKundenid.setFont(SWTResourceManager.getFont("Arial", 19, SWT.NORMAL));
@@ -97,13 +69,84 @@ public class Mitarbeiterliste {
 		lblKundenid.setAlignment(SWT.CENTER);
 		lblKundenid.setBounds(141, 50, 143, 38);
 
-		Label lblX = new Label(shlMehrereGleichnamigeKunden, SWT.NONE);
+		// III. Label: "X"
 		lblX.setText("X");
 		lblX.setForeground(SWTResourceManager.getColor(255, 255, 255));
 		lblX.setFont(SWTResourceManager.getFont("Arial", 19, SWT.NORMAL));
 		lblX.setBackground(SWTResourceManager.getColor(64, 0, 128));
 		lblX.setAlignment(SWT.CENTER);
 		lblX.setBounds(273, 50, 44, 38);
+	}
+
+	private void initButtons() {
+		// I. Button
+		btnNewButton.setFont(SWTResourceManager.getFont("Arial", 9, SWT.NORMAL));
+		btnNewButton.setText("Zur\u00FCck zum Hauptemen\u00FC");
+		btnNewButton.setBounds(111, 348, 271, 25);
 
 	}
+
+	private void initList() {
+		list.setTouchEnabled(true);
+		list.setBounds(65, 179, 359, 136);
+	}
+
+	@Override
+	public void assignElements() {
+		lblWelchenGenauMeinen = new Label(shell, SWT.NONE);
+		list = new List(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		lblKundenid = new Label(shell, SWT.NONE);
+		lblX = new Label(shell, SWT.NONE);
+		btnNewButton = new Button(shell, SWT.NONE);
+	}
+
+	@Override
+	public void dispose() {
+		lblWelchenGenauMeinen.dispose();
+		list.dispose();
+		lblKundenid.dispose();
+		lblX.dispose();
+		btnNewButton.dispose();
+	}
+
+	public Button getBtnNewButton() {
+		return btnNewButton;
+	}
+
+	public void setBtnNewButton(Button btnNewButton) {
+		this.btnNewButton = btnNewButton;
+	}
+
+	public Label getLblWelchenGenauMeinen() {
+		return lblWelchenGenauMeinen;
+	}
+
+	public void setLblWelchenGenauMeinen(Label lblWelchenGenauMeinen) {
+		this.lblWelchenGenauMeinen = lblWelchenGenauMeinen;
+	}
+
+	public List getList() {
+		return list;
+	}
+
+	public void setList(String[] inhalte) {
+		this.list.setItems(inhalte);
+	}
+
+	public Label getLblKundenid() {
+		return lblKundenid;
+	}
+
+	public void setLblKundenid(Label lblKundenid) {
+		this.lblKundenid = lblKundenid;
+	}
+
+	public Label getLblX() {
+		return lblX;
+	}
+
+	public void setLblX(Label lblX) {
+		this.lblX = lblX;
+	}
+
 }
