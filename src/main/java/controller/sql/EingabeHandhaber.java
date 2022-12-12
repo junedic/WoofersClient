e9f8a204a730d9c749ff3948248a347afc497723
junedic <julinedi@protonmail.com>
2022-12-12 22:14:22 +0100

    regex din

--- src/main/java/controller/sql/EingabeHandhaber.java
+++ src/main/java/controller/sql/EingabeHandhaber.java
@@ -10,7 +10,7 @@ public class EingabeHandhaber {
     public enum ErwarteterInput {
         Vorname("^[\\p{L}'][ \\p{L}'-]*[\\p{L}]$"),
         Nachname("^[\\p{L}'][ \\p{L}'-]*[\\p{L}]$"),
-        Telefonnummer("^([\\+][0-9]{1,3}[ \\.\\-])?([\\(]{1}[0-9]{1,6}[\\)])?([0-9 \\.\\-\\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$"),
+        Telefonnummer("^(\\+[0-9]{1,} |0)[1-9]{2,} [0-9]{2,}(\\-[0-9]{1,}|)$"),
         Email("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}");
         private String regex;
         ErwarteterInput(String regex) {
